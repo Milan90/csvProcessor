0f38ff041ca13633829a6966fb5f11fc2ca17c5a
Milan Nowak <milan.nowak90@gmail.com>
2019-02-08 13:38:03 +0100

    update convert_date and country_decder funcons clcka_mount and sortig function

--- csvProcessor.py
+++ csvProcessor.py
@@ -7,7 +7,7 @@ class Processor:
     def __init__(self, path):
         self.path = path
 
-    def csv_prcessr(self, input_file_path, output_file_path):
+    def csv_processor(self, input_file_path, output_file_path):
 
         with open(input_file_path, 'r', newline='') as inputFile:
             fieldnames = ['date', 'state name', 'impressions', 'CTR']
@@ -15,35 +15,76 @@ class Processor:
             input_data = list(input_reader)
 
             new_data_format = self.convert_date(input_data)
-            country_alpha3_code = self.country_decoder(input_data)
+            country_alpha3_code = self.country_decoder(new_data_format)
+            click_amount = self.click_amount(country_alpha3_code)
+            sorted_data = self.sorting(click_amount)
+
+        with open(output_file_path, "w", newline="") as outputFile:
+            fieldnames = ['date', 'country', 'impressions', 'number of clicks']
+            output_writer = csv.DictWriter(outputFile, fieldnames=fieldnames)
+            output_writer.writeheader()
+            for row in sorted_data:
+                output_writer.writerow(row)
 
     @staticmethod
     def convert_date(input_data):
 
-        new_date_list = []
-        new_date_format = []
+        for row in input_data:
+            try:
+                splitedDate = row['date'].split('/')
+                row['date'] = '-'.join([splitedDate[2], splitedDate[0], splitedDate[1]])
+            except IndexError:
+                return "Wrong date format. Should be dd/MM/YY"
 
-        for i in range(len(input_data)):
-            date = input_data[i][0]
-            splited_date = date.split("/")
-            new_date_list.append(splited_date[2])
-            new_date_list.append(splited_date[0])
-            new_date_list.append(splited_date[1])
-            separator = "-"
-            new_date_format.append(separator.join(new_date_list))
-            new_date_list = []
-
-        return new_date_format
+        return input_data
 
     @staticmethod
     def country_decoder(input_data):
-        country_alpha3_code = []
+
+        for row in input_data:
+            try:
+                subdivision = pycountry.subdivisions.lookup(row['state name'])
+                country = pycountry.countries.get(alpha_2=subdivision.country_code)
+                row['country'] = country.alpha_3
+                del row['state name']
+            except LookupError:
+                row['country'] = 'XXX'
+                del row['state name']
+
+        return input_data
+
+    @staticmethod
+    def click_amount(input_data):
         for i in range(len(input_data)):
 
             try:
-                subdivision = pycountry.subdivisions.lookup(input_data[i][1])
-                country = pycountry.countries.get(alpha_2=subdivision.country_code)
-                country_alpha3_code.append(country.alpha_3)
-            except LookupError:
-                country_alpha3_code.append("XXX")
-        return country_alpha3_code
+                cliks = round(int(input_data[i]['impressions']) * (float(input_data[i]['CTR'].rstrip("%")) / 100))
+                input_data[i]['number of clicks'] = cliks
+                del input_data[i]['CTR']
+            except KeyError:
+                print("key error")
+
+        return input_data
+
+    @staticmethod
+    def sorting(input_data):
+
+        sorted_data = sorted(input_data, key=lambda i: (i['date'], i['country']))
+
+        for i in range(len(sorted_data) + 1):
+            try:
+                if sorted_data[i]['date'] == sorted_data[i + 1]['date'] and \
+                        sorted_data[i]['country'] == sorted_data[i + 1]['country']:
+                    a = int(sorted_data[i]['impressions'])
+                    b = int(sorted_data[i + 1]['impressions'])
+                    c = int(sorted_data[i]['number of clicks'])
+                    d = int(sorted_data[i + 1]['number of clicks'])
+
+                    sorted_data[i]['impressions'] = a + b
+                    sorted_data[i]['number of clicks'] = c + d
+                    del sorted_data[i + 1]
+
+            except IndexError:
+                pass
+
+        return input_data
